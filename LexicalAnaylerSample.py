8b9c22cda41f56e195c8b80e461667f77db9646d
Antonio Oliveira <antoniovini47@gmail.com>
2024-11-11 16:36:32 -0300

    Lexical Analyser Check

---
+++ LexicalAnaylerSample.py
@@ -0,0 +1,10 @@
+def ePositivoOuNegativo(x):
+    if x > 0:
+        print("Positivo")
+    else:
+        print("Negativo")
+
+a = 10
+b = 20
+c = a + b
+print(c)
--- sourceCode.py
+++ sourceCode.py
@@ -1,11 +1,2 @@
-def saudar(nome):
-    print("Olá, " + nome + "!")
-
-def somar(a, b):
-    return a + b
-
-read = input("Digite seu nome: ")
-saudar(read)
-soma = input("Digite um número: ")
-soma2 = input("Digite outro número: ")
-print(somar(int(soma), int(soma2)))
+a = 10;
+b = 20;
